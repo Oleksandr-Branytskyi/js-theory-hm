ee52c7739c6dfc0ae9f10528f955b8aa8931c48f
Oleksandr Branytskyi <o.branytskyi@gmail.com>
2025-09-06 13:31:01 +0300

    js theory

---
+++ js/module-8.js
@@ -0,0 +1,44 @@
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+// parent.addEventListener("click", () => {
+//   console.log("Parent click handler");
+// });
+
+// child.addEventListener("click", () => {
+//   console.log("Child click handler");
+// });
+
+// descendant.addEventListener("click", () => {
+//   console.log("Descendant click handler");
+// });
+//********************************************* */
+// const parent = document.querySelector("#parent");
+
+// parent.addEventListener("click", (event) => {
+//   console.log("event.target: ", event.target);
+//   console.log("event.currentTarget: ", event.currentTarget);
+// });
+//********************************************* */
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+// parent.addEventListener("click", () => {
+//   alert(
+//     "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+
+// child.addEventListener("click", () => {
+//   alert(
+//     "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+
+// descendant.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   alert("Descendant click handler");
+// });
+//************************************************* */
